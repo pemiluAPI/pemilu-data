800131c330c913ebf188efad481e6e96b3d40c4b
ACHMAD RIFALDI <achmad.rifaldi64@gmail.com>
2016-05-12 15:55:05 +0700

    Add import file for periode

---
+++ dpr.go.id/import/periode.php
@@ -0,0 +1,39 @@
+<?php
+require_once 'database.php';
+
+$file = new SplFileObject("../periode.csv");
+$file->setFlags(SplFileObject::READ_CSV);
+
+try{
+  $access = new PDO($dsn, $user, $password);
+  $access->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+  $access->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}catch(PDOException $e){
+  echo "Error: " . $e->getMessage();
+}
+
+//drop the target table
+$access->query('TRUNCATE TABLE periode_dpr');
+
+$query = $access->prepare('INSERT INTO periode_dpr (nama, periode)
+               VALUES (:nama, :periode)
+               ON DUPLICATE KEY UPDATE
+               nama = VALUES(nama),
+               periode = VALUES(periode)');
+
+foreach ($file as $row) {
+    $row = array_filter($row);
+    if(!empty($row)){
+      list($nama, $periode) = $row;
+
+      //skip first row
+      if ($nama != 'nama_fraksi'){
+        $query->bindParam(':nama', $nama, PDO::PARAM_STR);
+        $query->bindParam(':periode', $periode, PDO::PARAM_STR);
+        $query->execute();
+
+        echo $nama . "\n";
+      }
+    }
+}
+?>
